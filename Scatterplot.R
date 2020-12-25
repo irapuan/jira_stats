8f4492b2ec41a72d056550a223271da0ebf0422f
ibottosso <ibottosso@gmail.com>
2020-12-25 22:26:16 +0100

    add percentile lines to the scatter chart

--- Scatterplot.R
+++ Scatterplot.R
@@ -57,11 +57,20 @@ p95 <- quantile(jira_data$cycletime, 0.95)
 p80 <- quantile(jira_data$cycletime, 0.80)
 p70 <- quantile(jira_data$cycletime, 0.70)
 
+
 ggplot(jira_data, aes(x=closed, y=cycletime)) + 
   geom_point(aes(col=issuetype))+
   geom_hline(yintercept=p95, linetype="dashed", color = "red")+
+  annotate("text", x = min (jira_data$closed), y = p95, color = "red" , label = "95th Percentile", vjust = -0.5, hjust = "inward") +
   geom_hline(yintercept=p80, linetype="dashed", color = "blue")+
-  geom_hline(yintercept=p70, linetype="dashed", color = "green")
+  annotate("text", x = min (jira_data$closed), y = p80 , color = "blue", label = "80th Percentile", vjust = -0.5, hjust = "inward") +
+  geom_hline(yintercept=p70, linetype="dashed", color = "green") +
+  annotate("text", x = min (jira_data$closed), y = p70 , color = "green", label = "70th Percentile", vjust = -0.5, hjust = "inward") 
+
+
+
+
+
 
 
 #ggplot(jira_data, aes(x=cycletime, fill=issuetype)) +
